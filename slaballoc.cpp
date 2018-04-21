bffc5119598d8c0380ec2babfae7dd9f17b82f08
saksham-mittal <mittalsaksham01@gmail.com>
2018-04-21 13:42:25 +0530

    Added myalloc function

--- slaballoc.cpp
+++ slaballoc.cpp
@@ -1,19 +1,32 @@
-struct header{
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct slab {
 	int totobj;
 	int freeobj;
+	struct object
 	struct slab* buck;
-	struct slab* nxt_slab;
+	struct slab* nxtSlab;
+};
 
+struct bucket {
+	int objSize;
+	struct slab* firstSlab;
 };
 
 struct object{
-	slab* parent_slab;
-	void 
+	struct slab* parentSlab;
+	int userData;		// will comment it later
 }
 
-slab *bucket[11];
+bucket bcktTable[11];
 
-void* mymalloc(unsigned int size){
+void addSlab(struct slab* ptr) {
+	struct slab 
+}
+
+void* mymalloc(int size){
 	// find the bucket to put
 	// check the first slab for free objects
 		// update the bitmap and
@@ -21,8 +34,29 @@ void* mymalloc(unsigned int size){
 	// else go to the next slab
 	// if no slab has free objects then create a new slab and then return the free space
 	// and update the bitmap
+	int index;
+	for(int i=0; i<11; i++) {
+		if(bcktTable[i].objSize > size) {
+			index = i;
+			break;
+		}
+	}
+	if(bcktTable.firstSlab == NULL) {
+		// Add a slab to the beggining
+		addSlab(bcktTable.firstSlab);
+	}
 }
 
 void myfree(void *ptr){
 	//go to the ptr and use the slab* to go the slab and update the bitmap
+}
+
+int main() {
+	for(int i=0; i<11; i++) {
+		bcktTable[i].objSize = pow(2, i + 2);
+		bcktTable[i].firstSlab = NULL;
+	}
+	int size;		// This is supposed to be given in bytes
+	cin >> size;
+	struct object* obj = (struct object*)mymalloc(size);
 }
